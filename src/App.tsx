383119e14bd4f1608136cf8cb64dc74c90c88cc1
Joy <joybarranis@Joys-MacBook-Pro.local>
2021-11-24 08:25:21 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from '@paire/card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card text="card from paire bit components!" />
+    </div>
+  );
+}
+
+export default App;
